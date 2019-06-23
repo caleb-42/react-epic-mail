fb4cbf57f459ed28f5ed77c99d6fa72f3035af0a
ewere <ewisco316@yahoo.com>
2019-06-23 05:45:39 +0100

    ch(travis): install jest testing framework
    - create mock folder
    - create test script command
    - write test to match snapshots
    - update readme
    
    [Finishes #166785008]

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./app.scss";
 
 const App = () => {
   return <h1 className="header">Hello</h1>;
 };
-
-ReactDOM.render(<App />, document.querySelector("#app"));
+export default App;
