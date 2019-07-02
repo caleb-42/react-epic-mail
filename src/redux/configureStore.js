9fd49fa65abf16218c7f06721e5a77df8d081968
ewere <ewisco316@yahoo.com>
2019-07-02 11:54:09 +0100

    chore(redux): setup redux workflow
    - create structure for reducers, actions and the store
    
    [Finishes #166785001]

---
+++ src/redux/configureStore.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+import rootReducer from './reducers';
+
+export default function configureStore(initializeState) {
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  return createStore(rootReducer, initializeState,
+    composeEnhancers(applyMiddleware(reduxImmutableStateInvariant()))
+  );
+}
